fcdd9e81ec13e4e06bed8d46d5847064659d81d7
Marcus Runge <marcus.runge@outlook.com>
2018-08-08 20:36:15 +0200

    old data read proc again...

--- MyDEFCON/Receiver/TcpActionReceiver.cs
+++ MyDEFCON/Receiver/TcpActionReceiver.cs
@@ -11,6 +11,7 @@ using SQLite;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using Android.Widget;
+using System.Text;
 
 namespace MyDEFCON.Receiver
 {
@@ -21,20 +22,20 @@ namespace MyDEFCON.Receiver
         SQLiteAsyncConnection _sqLiteAsyncConnection;
         IEventService _eventService;
         static bool _isAlreadyConnected = false;
-        TcpClient _tcpClient;
+        //TcpClient _tcpClient;
         //Context _context;
 
         public TcpActionReceiver()
         {
             _sqLiteAsyncConnection = ServiceLocator.Current.GetInstance<ISQLiteDependencies>().AsyncConnection;
             _eventService = ServiceLocator.Current.GetInstance<IEventService>();
-            _tcpClient = new TcpClient();
+            //_tcpClient = new TcpClient();
         }
 
         public async override void OnReceive(Context context, Intent intent)
         {
             //_context = context;
-            if (!_isAlreadyConnected) await Task<string>.Run(async () =>
+            /*if (!_isAlreadyConnected) await Task<string>.Run(async () =>
               {
                   //Toast.MakeText(context, "New Checklist Update received...", ToastLength.Short).Show();
                   _isAlreadyConnected = true;
@@ -54,14 +55,33 @@ namespace MyDEFCON.Receiver
                       return result;
                   }
                   catch { return ""; }
-              }).ContinueWith(UpdateDatabase);
+              }).ContinueWith(UpdateDatabase);*/
+            if (!_isAlreadyConnected)
+            {
+                _isAlreadyConnected = true;
+                var remoteEndPointAddress = intent.GetStringExtra("RemoteEndPointAddress");
+                TcpClient tcpClient = new TcpClient(remoteEndPointAddress, 4537);
+                StringBuilder stringBuilder = new StringBuilder();
+                using (NetworkStream networkStream = tcpClient.GetStream())
+                {
+                    byte[] receiveBuffer = new byte[tcpClient.ReceiveBufferSize];
+                    int bytes = -1;
+                    do
+                    {
+                        bytes = await networkStream.ReadAsync(receiveBuffer, 0, receiveBuffer.Length);
+                        stringBuilder.Append(Encoding.ASCII.GetString(receiveBuffer));
+                        if (stringBuilder.ToString().IndexOf("\0") != -1) break;
+                    } while (bytes != 0);
+                }
+                await UpdateDatabase(stringBuilder.ToString());
+            }
         }
 
-        private async void UpdateDatabase(Task<string> obj)
+        private async Task UpdateDatabase(string jsonString)
         {
             try
             {
-                var checkListEntries = JsonConvert.DeserializeObject<List<CheckListEntry>>(obj.Result);
+                var checkListEntries = JsonConvert.DeserializeObject<List<CheckListEntry>>(jsonString);
                 foreach (var checkListEntry in checkListEntries)
                 {
                     CheckListEntry foundCheckListEntry = (await _sqLiteAsyncConnection.FindAsync<CheckListEntry>(c => c.UnixTimeStampCreated == checkListEntry.UnixTimeStampCreated));
