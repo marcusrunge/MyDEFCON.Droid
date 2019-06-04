e95cdba9b5c471d168b57c1eeb44231b638340e4
Marcus Runge <marcus.runge@outlook.com>
2019-06-04 21:09:19 +0200

    ADDED ForegroundService

---
+++ MyDEFCON/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyDEFCON
+{
+    public static class Constants
+    {
+        public const int SERVICE_RUNNING_NOTIFICATION_ID = 10000;
+        public const string SERVICE_STARTED_KEY = "has_service_been_started";
+        public const string BROADCAST_MESSAGE_KEY = "broadcast_message";
+        public const string NOTIFICATION_BROADCAST_ACTION = "com.marcusrunge.MyDEFCON.Notification.Action";
+        public const string ACTION_START_SERVICE = "com.marcusrunge.MyDEFCON.action.START_SERVICE";
+        public const string ACTION_STOP_SERVICE = "com.marcusrunge.MyDEFCON.action.STOP_SERVICE";
+        public const string ACTION_MAIN_ACTIVITY = "com.marcusrunge.MyDEFCON.action.MAIN_ACTIVITY";
+    }
+}
---
+++ MyDEFCON/Services/ForegroundService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MyDEFCON.Services
+{
+    [Service]
+    public class ForegroundService : Service
+    {
+        bool _isStarted;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            //return base.OnStartCommand(intent, flags, startId);
+            if (intent.Action.Equals(Constants.ACTION_START_SERVICE))
+            {
+                if (_isStarted)
+                {
+                }
+                else
+                {
+                    RegisterForegroundService();
+                    _isStarted = true;
+                }
+            }
+            else if (intent.Action.Equals(Constants.ACTION_STOP_SERVICE))
+            {
+                StopForeground(true);
+                StopSelf();
+                _isStarted = false;
+
+            }
+
+            // This tells Android not to restart the service if it is killed to reclaim resources.
+            return StartCommandResult.Sticky;
+        }        
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnDestroy()
+        {
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.Cancel(Constants.SERVICE_RUNNING_NOTIFICATION_ID);
+            _isStarted = false;
+            base.OnDestroy();
+        }
+
+        void RegisterForegroundService()
+        {
+            var notification = new Notification.Builder(this)
+                .SetContentTitle(Resources.GetString(Resource.String.app_name))
+                .SetContentText(Resources.GetString(Resource.String.notification_text))
+                .SetSmallIcon(Resource.Drawable.ic_stat_5)
+                .SetContentIntent(BuildIntentToShowMainActivity())
+                .SetOngoing(true)
+                .Build();
+
+
+            // Enlist this instance of the service as a foreground service
+            StartForeground(Constants.SERVICE_RUNNING_NOTIFICATION_ID, notification);
+        }
+
+        PendingIntent BuildIntentToShowMainActivity()
+        {
+            var notificationIntent = new Intent(this, typeof(MainActivity));
+            notificationIntent.SetAction(Constants.ACTION_MAIN_ACTIVITY);
+            notificationIntent.SetFlags(ActivityFlags.SingleTop | ActivityFlags.ClearTask);
+            notificationIntent.PutExtra(Constants.SERVICE_STARTED_KEY, true);
+
+            var pendingIntent = PendingIntent.GetActivity(this, 0, notificationIntent, PendingIntentFlags.UpdateCurrent);
+            return pendingIntent;
+        }
+    }
+}
