aef982f7f8ca11bc142710dbf71237b8345139c2
Marcus Runge <marcus.runge@outlook.com>
2022-04-15 16:02:49 +0200

    added MainViewModel

--- app/src/main/java/com/marcusrunge/mydefcon/MainActivity.kt
+++ app/src/main/java/com/marcusrunge/mydefcon/MainActivity.kt
@@ -1,31 +1,39 @@
 package com.marcusrunge.mydefcon
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marcusrunge.mydefcon.databinding.ActivityMainBinding
+import com.marcusrunge.mydefcon.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/com/marcusrunge/mydefcon/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.marcusrunge.mydefcon.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    init {
+
+    }
+}
