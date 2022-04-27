70aa392cd41919f95b15b30cfcead4af59666782
Marcus Runge <marcus.runge@outlook.com>
2022-04-27 19:38:35 +0200

    added action menu

--- app/src/main/kotlin/com/marcusrunge/mydefcon/MainActivity.kt
+++ app/src/main/kotlin/com/marcusrunge/mydefcon/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.marcusrunge.mydefcon
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.marcusrunge.mydefcon.databinding.ActivityMainBinding
 import com.marcusrunge.mydefcon.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,5 +25,40 @@ class MainActivity : AppCompatActivity() {
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            else -> return when (item.itemId) {
+                R.id.action_statusshare -> {
+                    true
+                }
+                R.id.action_listsync -> {
+                    true
+                }
+                R.id.navigation_settings -> {
+                    true
+                }
+                R.id.navigation_privacy -> {
+                    true
+                }
+                R.id.navigation_licenses -> {
+                    true
+                }
+                R.id.navigation_terms -> {
+                    true
+                }
+                android.R.id.home -> {
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
