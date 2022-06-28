943c211df2fc3fced35e09b60fd2f910682d2a0d
Marcus Runge <marcus.runge@outlook.com>
2022-06-28 20:23:21 +0200

    refactored colors

--- app/src/main/kotlin/com/marcusrunge/mydefcon/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/marcusrunge/mydefcon/ui/settings/SettingsFragment.kt
@@ -1,6 +1,12 @@
 package com.marcusrunge.mydefcon.ui.settings
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginStart
+import androidx.core.view.setPadding
 import androidx.preference.PreferenceFragmentCompat
 import com.marcusrunge.mydefcon.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,5 +15,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view.setBackgroundColor(Color.argb(0.698f, 0f, 0f,0f))
+        return view
     }
 }
