f3c12ff4636235a8801fc1c98c4ea620d17f30d5
Marcus Runge <marcus.runge@outlook.com>
2024-05-31 15:25:29 +0200

    gradle

--- notifications/build.gradle.kts
+++ notifications/build.gradle.kts
@@ -38,7 +38,7 @@ sourceSets.configureEach {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation(project(":core"))
     testImplementation("junit:junit:4.13.2")
