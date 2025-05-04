79421088911a5510b149ed8116c0fa24e97bab35
Marcus Runge <marcus.runge@outlook.com>
2025-05-04 19:33:29 +0200

    added defcon group

--- firebase/src/main/kotlin/com/marcusrunge/mydefcon/firebase/interfaces/Firestore.kt
+++ firebase/src/main/kotlin/com/marcusrunge/mydefcon/firebase/interfaces/Firestore.kt
@@ -1,4 +1,7 @@
 package com.marcusrunge.mydefcon.firebase.interfaces
 
+import com.marcusrunge.mydefcon.firebase.documents.DefconGroup
+
 interface Firestore {
+    suspend fun getDefconGroup() : DefconGroup
 }
