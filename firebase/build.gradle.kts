4d3294c52a2f83a40c98bffe27250322c228d205
Marcus Runge <marcus.runge@outlook.com>
2025-07-31 21:38:06 +0200

    updated gradle

--- firebase/build.gradle.kts
+++ firebase/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:26.0.0")
     implementation("com.google.firebase:firebase-messaging:25.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
