84a6a64e50429e09c23c4e528726c57e016de653
Marcus Runge <marcus.runge@outlook.com>
2025-11-12 17:58:00 +0100

    updated gradle

--- app/src/main/kotlin/com/marcusrunge/mydefcon/utils/TileUtil.kt
+++ app/src/main/kotlin/com/marcusrunge/mydefcon/utils/TileUtil.kt
@@ -9,6 +9,7 @@ import android.graphics.PixelFormat
 import android.graphics.Shader
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.createBitmap
 
 
 class TileUtil(drawable: Drawable, tileMode: Shader.TileMode) : Drawable() {
@@ -40,10 +41,7 @@ class TileUtil(drawable: Drawable, tileMode: Shader.TileMode) : Drawable() {
         if (drawable is BitmapDrawable) {
             return drawable.bitmap
         }
-        val bitmap = Bitmap.createBitmap(
-            drawable.intrinsicWidth, drawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
+        val bitmap = createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight)
         val canvas = Canvas(bitmap)
         drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
         drawable.draw(canvas)
