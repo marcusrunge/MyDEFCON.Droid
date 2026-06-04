ed2a91e099a4c8eb2a34cf980361b1db34d6ccef
Marcus Runge <marcus.runge@outlook.com>
2026-06-04 19:31:07 +0200

    updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.1.0" apply false
-    id("com.android.library") version "9.1.0" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     id("org.jetbrains.kotlin.jvm") version "2.3.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20" apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.9" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("com.google.android.gms.oss-licenses-plugin") version "0.11.0" apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+    id("com.google.android.gms.oss-licenses-plugin") version "0.12.0" apply false
 }
